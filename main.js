f4cbf4ba1a40ac8623ae81affa965f57a9ee7636
Zbigniew Słomian <zibi.slomian@gmail.com>
2021-01-11 22:29:43 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,16 @@
+/* alert(`uwaga!!!!`) */
+const wiek = 47;
+const Zbyszek = "Zbyszek";
+const Piotrek = "Piotrek";
+const wiek_P = 30;
+console.log('witaj mam lat:', wiek);
+console.log('kto jest starszy?');
+
+
+if (wiek > wiek_P) {
+    console.log(`starszy jest: ${Zbyszek} bo ma ${wiek} lat.`);
+} else {
+    console.log(`starszy jest: ${Piotrek} bo ma ${wiek_P} lat.`);
+}
+
+
