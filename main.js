cce057a90dbc9693a1416005e03b685b5f8ba684
Zbigniew Słomian <zibi.slomian@gmail.com>
2021-01-26 21:00:26 +0100

    dodane stylowanie zgodne z BEM, oraz wstrzyknięcie kodu za pomocą js

--- main.js
+++ main.js
@@ -13,4 +13,9 @@ if (wiek > wiek_P) {
     console.log(`starszy jest: ${Piotrek} bo ma ${wiek_P} lat.`);
 }
 
+const paragraf_js = document.querySelector('.week__notes--js')
 
+paragraf_js.innerHTML = `- document.QuerySelector('.selector') - dostanie się do elementu<br>
+- .innerHTML wstrzyknięcie do wewnętrznego html,<br>
+- console.log(elenentxxx.innerHTML) - wypisuje w konsoli wewnętrzny
+html`
